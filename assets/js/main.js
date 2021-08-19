f9c6e83e4d6ee079da080352dd99c92806c1a7b8
liohumb <helloliohumb@gmail.com>
2021-08-19 14:57:55 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 function scrollHeader(){
     const header = document.getElementById('header')
-    
+
     if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
