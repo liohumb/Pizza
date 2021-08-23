9618f9f503a9fd42eb9d206e56d94c289befc45a
GuillaumeLariviere <guillaume.lariviere59@gmail.com>
2021-08-23 16:32:34 +0200

    nouveau panier

--- testPanier.php
+++ testPanier.php
@@ -18,14 +18,17 @@ switch($_GET["action"]) {
 }
 }
 ?>
+
+
+
 <!doctype html>
 <html lang=fr>
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <link rel="stylesheet" href="assets/css/panier.css">
     <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/panier.css">
     <title>Pizza</title>
 </head>
 <body>
@@ -39,7 +42,7 @@ switch($_GET["action"]) {
                     <a href="index.html" class="nav__link">Acceuil</a>
                 </li>
                 <li class="nav__item">
-                    <a href="index.html" class="nav__link">À propos</a>
+                    <a href="index.html#about" class="nav__link">À propos</a>
                 </li>
                 <li class="nav__item">
                     <a href="menu.html" class="nav__link">Le Menu</a>
@@ -52,74 +55,94 @@ switch($_GET["action"]) {
             <i class="uil uil-times nav__close" id="nav-close"></i>
         </div>
 
-        <a href="panier.php" class="nav__link nav__cart"><i class="uil uil-pizza-slice cart_logo"></i></a>
+        <a href="panier.html" class="nav__link nav__cart"><i class="uil uil-pizza-slice cart_logo"></i></a>
+
 
         <div class="nav__toggle" id="nav-toggle">
             <i class="uil uil-bars"></i>
         </div>
     </nav>
+
+    <div class="connexion">
+        <a href="inscription.html" class="btn-conn">Inscription</a>
+        |
+        <a href="connexion.html" class="btn-conn">Connexion</a>
+    </div>
+
+    <!--    <div class="connexion-bis">
+            <a href="" class="btn-conn">Mon compte</a>
+            |
+            <a href="" class="btn-conn">Déconnexion</a>
+        </div>
+    -->
 </header>
 
-<section class="cart section">
-    <div class="panier__container container grid">
-        <div class="panier__content grid">
-            <h3 class="panier__title">Votre panier</h3>
-            <a href="panier.php?action=vider" id="btnVider">Vider Panier</a>
-             <?php
+<main class="home" id="home">
+    <section class="about section">
+    <?php
                  if(isset($_SESSION["Panier_item"])){
                     $quantityTotal = 0;
                     $PrxTotal = 0;
-                 
-            ?>
-            <!-- <table class="tbl-panier" cellpadding="10" cellspacing="1">
-                <tbody>
-                    <tr>
-                        <th>Nom</th>
-                        <th>Code</th>
-                        <th>Quantité</th>
-                        <th>Prix unitaire</th>
-                        <th>Prix</th>
-                        <th>Supprimé</th>
-                    </tr> -->
+                 ?>
+        <div class="about__container container grid">
                     <?php
-                    foreach($_SESSION["Panier_item"]as $item){
+                       foreach($_SESSION["Panier_item"]as $item){
                         $item_prix = $item["quantity"]*$item["price"];
-                        ?>
-                        <tr>
-                        <td><img src="<?php echo $item["image"];?>" class="panier_item_image" alt=""><?php echo $item["name"]?></td>
-                        <td><?php echo $item["id"]; ?></td>
-                        <td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
-                        <td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
-                        <td  style="text-align:right;"><?php echo "$ ". number_format($item_prix,2); ?></td>
-                        <td style="text-align:center;"><a href="panier.php?action=retire&id=<?php echo $item["id"]; ?>" class="btnRetireAction"><img src="icon-delete.png" alt="retire Item" /></a></td>
-				        </tr>
-                    <?php
-                    $quantityTotal += $item["quantity"];
-                    $PrxTotal += ($item["price"]*$item["quantity"]);
-                    }
                     ?>
-                    <tr>
-                        <td class ="right">Total:</td>
-                        <td class ="right"><?php echo $quantityTotal;?></td>
-                        <td class="right"><strong><?php echo"$ ".number_format($PrxTotal, 2); ?></strong></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-            <?php
-                }
-                else{
-                    ?>
-                    <div class ="no-records">Votre panier est vide </div>
-                    <?php
-                } 
-                ?>
+
+            <div class="about__img">
+                <div class="about__img-overlay">
+                    <img src="<?php echo $item["image"];?>" alt="" class="about__img-two">
+                </div>
+            </div>
+
+            <div class="contact__data">
+                <h1 class="product-name"><?php echo $item["name"]?></h1>
+                <h2 class="product-price">10€90</h2>
+                <ul class="preferences">
+                    <li>
+                        <span class="list-name">Quantité :</span>
+                        <a href="" class="list-name"><i class="uil uil-minus"></i></a>
+                        <span class="list-name">1</span>
+                        <a href="" class="list-name"><i class="uil uil-plus"></i></a>
+
+                    </li>
+                </ul>
+                <span class="cart-subtitle">Prix unitaire : <b>10€90</b></span>
+            </div>
         </div>
-    </div>
 
+        <div class="recap">
+            <div class="recap__title">
+                <h2 class="section__title menu_suggestion">Récapitulatif</h2>
+            </div>
+            <div class="recap__total">
+                <h2 class="section__title menu_suggestion">Sous-total : <b>10€90</b></h2>
+                <h2 class="section__title menu_suggestion">Livraison : <b>3€50</b></h2>
+                <br>
+                <h2 class="section__title menu_suggestion">Total : <b>14€40</b></h2>
 
-</section>
+            </div>
+        </div>
 
+        <div class="btn-choix">
+            <a href="menu.html" class="button btn-ret-aj">Retour au menu</a>
+            <a href="#" class="button-supp">Vider le panier</a>
+            <a href="#" class="button btn-ret-aj">Valider mon panier</a>
+        </div>
+    </section>
+    <!--
+    <section class="about section">
+        <div class="empty__cart">
+                <h1 class="product-name">Votre Panier est vide</h1>
+        </div>
+        <div class="btn-choix">
+            <a href="menu.html" class="button btn-ret-aj">Retour au menu</a>
+        </div>
+    </section>
+    -->
+
+</main>
 <footer class="footer section">
     <div class="footer__container container grid">
         <div class="footer__content grid">
@@ -167,4 +190,3 @@ switch($_GET["action"]) {
 <script src="assets/js/main.js"></script>
 </body>
 </html>
-
