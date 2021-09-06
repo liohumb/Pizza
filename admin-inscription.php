72111a3b55cba52824c95e1c3ea30c4c5c2a7bea
GuillaumeLariviere <guillaume.lariviere59@gmail.com>
2021-09-06 16:27:37 +0200

    ajout d'un employée admin ok

--- admin-inscription.php
+++ admin-inscription.php
@@ -17,7 +17,7 @@ if(isset($_POST['adminEmail'])){
         $email = $_POST['email'];
         $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
         $phone = $_POST['phone'];
-        $poste = $_POST['poste'];
+        $poste = $_POST['job'];
 
         $sql ="INSERT INTO `worker`(`first_name`, `last_name`, `email`, `pass`, `phone`, `poste`) VALUES (?,?,?,?,?,?)";
         $stmt = $db_handle->connectDB()->prepare($sql);
@@ -118,9 +118,9 @@ if(isset($_POST['adminEmail'])){
                     <label for="job">
                         <select name="job" id="job_input" required>
                             <option disabled hidden selected>Choisissez un poste</option>
-                            <option value="">Employé(e) de cuisine</option>
-                            <option value="">Employé(e) de salle</option>
-                            <option value="">Livreur</option>
+                            <option value="Employé(e) de cuisine">Employé(e) de cuisine</option>
+                            <option value="Employé(e) de salle">Employé(e) de salle</option>
+                            <option value="Livreur">Livreur</option>
                         </select>
                     </label>
                 </div>
